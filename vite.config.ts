74bc1749b76875d5b92947048f1d89d524e388f9
Khoi Hoang <lahoangkhoi.ck@gmail.com>
2025-12-11 17:06:36 +0700

    add https

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 
 export default defineConfig(({ mode }) => {
@@ -8,8 +9,9 @@ export default defineConfig(({ mode }) => {
       server: {
         port: 3000,
         host: '0.0.0.0',
+        https: true, // Enable HTTPS for camera access on mobile
       },
-      plugins: [],
+      plugins: [basicSsl()],
       define: {
       },
       resolve: {
